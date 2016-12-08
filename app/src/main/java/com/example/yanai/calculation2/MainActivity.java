a1feaa5f5a8913877fc76b3182f0024dcfeef771
yanai <yanai@rio.ne.jp>
2016-12-08 17:39:45 +0900

    ボタンの設定とtextViewの値をを変更

--- app/src/main/java/com/example/yanai/calculation2/MainActivity.java
+++ app/src/main/java/com/example/yanai/calculation2/MainActivity.java
@@ -2,12 +2,123 @@ package com.example.yanai.calculation2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    //宣言
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button0;
+    private Button button_divide;
+    private Button button_multiply ;
+    private Button button_minus;
+    private Button button_plus;
+    private Button button_clear;
+    private Button button_equal;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(this);
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(this);
+
+        button7 = (Button) findViewById(R.id.button7);
+        button7.setOnClickListener(this);
+
+        button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(this);
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(this);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button0.setOnClickListener(this);
+
+        button_divide = (Button) findViewById(R.id.button_divide);
+        button_divide.setOnClickListener(this);
+
+        button_multiply = (Button) findViewById(R.id.button_multiply);
+        button_multiply.setOnClickListener(this);
+
+        button_minus = (Button) findViewById(R.id.button_minus);
+        button_minus.setOnClickListener(this);
+
+        button_plus = (Button) findViewById(R.id.button_plus);
+        button_plus.setOnClickListener(this);
+
+        button_clear = (Button) findViewById(R.id.button_clear);
+        button_clear.setOnClickListener(this);
+
+        button_equal = (Button) findViewById(R.id.button_equal);
+        button_equal.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        //
+        switch (v.getId()){
+            case R.id.button0:
+                textView.setText(textView.getText() + "0");
+                break;
+            case R.id.button1:
+                textView.setText(textView.getText() + "1");
+                break;
+            case R.id.button2:
+                textView.setText(textView.getText() + "2");
+                break;
+            case R.id.button3:
+                textView.setText(textView.getText() + "3");
+                break;
+            case R.id.button4:
+                textView.setText(textView.getText() + "4");
+                break;
+            case R.id.button5:
+                textView.setText(textView.getText() + "5");
+                break;
+            case R.id.button6:
+                textView.setText(textView.getText() + "6");
+                break;
+            case R.id.button7:
+                textView.setText(textView.getText() + "7");
+                break;
+            case R.id.button8:
+                textView.setText(textView.getText() + "8");
+                break;
+            case R.id.button9:
+                textView.setText(textView.getText() + "9");
+                break;
+        }
     }
 }
